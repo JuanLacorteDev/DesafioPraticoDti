d5cc9c74fc98d27d18339396ba8ee2cd487c31fa
juan henrique <lacorte07@hotmail.com>
2021-08-31 17:54:29 -0300

    estrutura base

--- AppFrontDtiDesafio/src/app/app.module.ts
+++ AppFrontDtiDesafio/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
   providers: [],
   bootstrap: [AppComponent]
